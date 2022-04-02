151fdbf44613cea7506c8bd19352a81f5b50f779
Mihret Tadesse <mihrettadesse@Mihrets-MBP.attlocal.net>
2022-04-02 15:43:27 -0400

    Update DogGenetics code

--- src/basicprogrammingconcepts/DogGenetics.java
+++ src/basicprogrammingconcepts/DogGenetics.java
@@ -1,62 +1,59 @@
-
 package basicprogrammingconcepts;
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class DogGenetics {
-    
-    public static void main(String []args){
-  
-Scanner sc = new Scanner(System.in);
-System.out.println("What is your dog's name?");
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your dog's name?");
 // Entering name of dog
-String name = sc.nextLine();
-  
-System.out.println("Well then, I have this highly reliable report on " + name + "'s prestigious background right here.");
-  
-System.out.println("\n\nSir " + name + " is\n\n");
-  
+        String name = sc.nextLine();
+
+        System.out.println("Well then, I have this highly reliable report on " + name + "'s prestigious background right here.");
+
+        System.out.println("\n\nSir " + name + " is\n\n");
+
 //Generating random numbers
-Random ran = new Random();
-int sum = 0;
-int breed1 = 0;
-int breed2 = 0;
-int breed3 = 0;
-int breed4 = 0;
-int breed5 = 0;
-int maxPercentage=100;
+        Random ran = new Random();
+        int sum = 0;
+        int breed1 = 0;
+        int breed2 = 0;
+        int breed3 = 0;
+        int breed4 = 0;
+        int breed5 = 0;
+        int maxPercentage = 100;
 
+        while (sum != 100) {
+            breed1 = ran.nextInt(maxPercentage);
 
-while(sum != 100)
-{
-breed1 = ran.nextInt(maxPercentage);
+            maxPercentage = maxPercentage - breed1;
 
-maxPercentage=maxPercentage-breed1;
-  
- breed2 = ran.nextInt(maxPercentage);
- 
-maxPercentage=maxPercentage-breed2;
-  
- breed3= ran.nextInt(maxPercentage);
- maxPercentage=maxPercentage-breed3;
+            breed2 = ran.nextInt(maxPercentage);
 
-breed4 = ran.nextInt(maxPercentage);
-  
-breed5 = maxPercentage;
+            maxPercentage = maxPercentage - breed2;
 
-sum = breed1 + breed2 +breed3 + breed4 + breed5;
+            breed3 = ran.nextInt(maxPercentage);
+            maxPercentage = maxPercentage - breed3;
+
+            breed4 = ran.nextInt(maxPercentage);
+            
+            maxPercentage = maxPercentage - breed4;
+
+            breed5 = maxPercentage;
+
+            sum = breed1 + breed2 + breed3 + breed4 + breed5;
+        }
+
+        System.out.println(breed1 + "% St. Bernard");
+        System.out.println(breed2 + "% Chihuahua");
+        System.out.println(breed3 + "% Dramatic RedNosed Asian Pug");
+        System.out.println(breed4 + "% Common Cur");
+        System.out.println(breed5 + "% King Doberman");
+
+        System.out.println("\n\nWow, that's QUITE the dog!");
+
+    }
 }
-  
-System.out.println(breed1 + "% St. Bernard");
-System.out.println(breed2 + "% Chihuahua");
-System.out.println(breed3+ "% Dramatic RedNosed Asian Pug");
-System.out.println(breed4+ "% Common Cur");
-System.out.println(breed5+ "% King Doberman");
-  
-System.out.println("\n\nWow, that's QUITE the dog!");
-  
-}
-}
-    
-
